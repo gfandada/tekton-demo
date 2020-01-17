fe3f8453d54298817bd6523b03f62077c94f6fd2
gfandada <gfandada@gmail.com>
2020-01-17 10:27:14 +0800

    init projection

---
+++ src/httpserver.go
@@ -0,0 +1,34 @@
+/*
+Copyright 2019 fanlin@oppo.com.
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Hello world received a request.")
+	target := os.Getenv("TARGET")
+	if target == "" {
+		target = "no target"
+	}
+	fmt.Fprintf(w, "%s!\n", target)
+}
+
+func main() {
+	log.Print("http server started.")
+
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
